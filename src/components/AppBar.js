739b9eca8cdae19dd85698bb2d701bb2cc47c6f0
Teyeb <hussenab@gmail.com>
2023-01-09 22:19:36 +0200

    navbar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,38 +1,134 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
-import { Button } from '@mui/material';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
-export default function ButtonAppBar() {
+const drawerWidth = 240;
+const navItems = [
+  {
+    id: '11',
+    name: 'Home',
+    url: '/',
+
+  },
+  {
+    id: '12',
+    name: 'CreateStudent',
+    url: '/createstudent',
+
+  },
+  {
+    id: '13',
+    name: 'CheckPhone',
+    url: '/checkphone',
+
+  },
+];
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.id} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <Link to={`${item.url}`}><ListItemText primary={item.name} /></Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
           >
-            <Link to="/"><MenuIcon /></Link>
+            <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div"  sx={{ flexGrow: 1 }}>
-            Java-React Full Stack - app 
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
           </Typography>
-          
-          <Link to="/createStudent">Create Student</Link>
-          <Button><Link to="/checkPhone">Check Phone</Link></Button>
-          
-          
-          {/* <Button color="inherit">Login</Button> */}
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item.id} sx={{ color: '#fff' }}>
+                <Link to={`${item.url}`}>{item.name}</Link>
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>
+          <h3>Java-React full stack app </h3>
+        </Typography>
+      </Box>
     </Box>
   );
 }
+
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
